bb683a6d8447efe4caa9588a3d67bc528da853ee
smithadam <sogaxili@gmail.com>
2018-12-12 10:36:37 -0500

    继续添加几行生成虚拟数据的代码

--- resource/app/fake.py
+++ resource/app/fake.py
@@ -35,6 +35,9 @@ class MakeFakerData():
 
     @staticmethod
     def make_user(num = 100):
+        """设置用户id为1的为站长，id小于10的为管理员"""
+        role_ids = Role.query.with_entities(Role.id).all()
+        role_ids = [ x[0] for x in role_ids ]
         faker_users = [User(
             email = fake_zh.email(),
             username = fake_en.name(),
@@ -43,7 +46,32 @@ class MakeFakerData():
             name = fake_zh.name(),
             location = fake_zh.address(),
             about_me = fake_zh.text(max_nb_chars=100),
-            role_id = random.choice([])
+            role_id = random.choice(role_ids)
         ) for _ in range(num)]
         db.session.add_all(faker_users)
         db.session.commit()
+
+    @staticmethod
+    def make_permission():
+        """创建权限，以及插入角色对应的权限"""
+        pass
+
+    @staticmethod
+    def make_category():
+        """创建资源标签"""
+        pass
+
+    @staticmethod
+    def make_mediatype():
+        """创建媒体资源类型"""
+        pass
+
+    @staticmethod
+    def make_resource(num=1000):
+        """创建资源"""
+        pass
+
+    @staticmethod
+    def make_project(num=100):
+        """创建资源专题，以及专题对应的资源"""
+        pass
