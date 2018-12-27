b786c1f85f40a7bea1cc02a5ece6e79e2abedd7b
smithadam <sogaixli@gmail.com>
2018-12-27 10:53:37 +0800

    重新更正数据库版本控制

--- resource/app/__init__.py
+++ resource/app/__init__.py
@@ -3,16 +3,9 @@
 
 from flask import Flask
 from config import config
-
-def create_app(config_name):
-    app = Flask(__name__)
-    app.config.from_object(config[config_name])
-    config[config_name].init_app(app)
-
-    register_blueprint(app)
-    register_extensions(app)
-
-    return app
+from os import getenv
+from logging import basicConfig, INFO, info
+from datetime import date
 
 def register_blueprint(app):
     from app.api import api_blueprint
@@ -22,3 +15,27 @@ def register_extensions(app):
     from app.extensions import db, migrate
     db.init_app(app)
     migrate.init_app(app, db)
+
+def init_log():
+    today_str = date.today().strftime('%Y-%m-%d')
+    basicConfig(filename='./log/resource-' + today_str + '.log', level=INFO)
+
+def start_remote_debug():
+    import ptvsd
+    ptvsd.enable_attach(address=('0.0.0.0', 8899), redirect_output=True)
+
+def create_app(config_name):
+    """
+    初始化app，注册蓝图，注册扩展，启动日志
+    """
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    register_blueprint(app)
+    register_extensions(app)
+    init_log()
+
+    return app
+
+app = create_app(getenv('FlASK_CONFIG') or 'default')
--- resource/run.py
+++ resource/run.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python3
 # -*- conding:utf8 -*-
 
-from app import create_app
+from app import app
 from app.extensions import db
 from app.models.permission import Permission
 from app.models.user import User
 from app.models.role import Role
 from app.lib.fake import FakerData
-from config import config
-from logging import basicConfig, INFO, info
-from os import getenv
-from datetime import date
-# import ptvsd
-
-# ptvsd.enable_attach(address=('0.0.0.0', 8899), redirect_output=True)
-
-app = create_app(getenv('FlASK_CONFIG') or 'default')
-today_str = date.today().strftime('%Y-%m-%d')
-basicConfig(filename='./log/resource-' + today_str + '.log', level=INFO)
+from app.lib.command import init_db
 
 
-# 集成 Python shell
 @app.shell_context_processor
 def make_shell_context():
+    """集成 Python shell""" 
     return dict(app=app, db=db, Permission=Permission, User=User, Role=Role, FakerData=FakerData)
 
 
